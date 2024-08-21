ff2bd656583dae8e28de5cfce7a029d473c8fdd0
sai <ojiip95@gmail.com>
2024-08-21 18:29:38 +0200

    build: task 1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""  1. FIFO caching """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """FIFOCache class implements a First-In-First-Out caching system.
+
+        Inherits from the BaseCaching class,
+            providing a caching mechanism that discards
+            the oldest item when the cache reaches its maximum capacity.
+
+        Methods:
+            __init__(): Initializes the FIFOCache instance.
+            put(key, item): Stores an item in the cache with the given key,
+                following the FIFO policy.
+            get(key): Retrieves an item from the cache using the given key.
+    """
+    def __init__(self):
+        """
+        Initializes the FIFOCache instance.
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+            Stores an item in the cache with the given key,
+                following the FIFO policy.
+            If the cache is full, the oldest item (first item added)
+                is discarded before storing the new item.
+
+            Args:
+                key (str): The key to use for storing the item.
+                item (any): The item to store in the cache.
+        """
+        if key and item:
+            self.cache_data[key] = item
+
+        if len(self.cache_data) > self.MAX_ITEMS:
+            first_key = list(self.cache_data)[0]
+            del self.cache_data[first_key]
+            print(f"DISCARD: {first_key}")
+
+    def get(self, key):
+        """Retrieves an item from the cache using the given key.
+
+            Args:
+                key (str): The key to use for retrieving the item.
+
+            Returns:
+                The item stored in the cache with the given key,
+                    or None if not found.
+        """
+        super.get(key)
