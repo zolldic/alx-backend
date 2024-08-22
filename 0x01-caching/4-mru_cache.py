09290fe43ad616c9b81f1f546ffaeaebb6515635
sai <ojiip95@gmail.com>
2024-08-22 15:40:56 +0200

    refactor(put): pass if key and item is none

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -20,16 +20,17 @@ class MRUCache(BaseCaching):
                 key (any): The key to store.
                 item (any): The value to associate with the key.
         """
-        if key in self.cache_data:
-            self.order.remove(key)
+        if key and item:
+            if key in self.cache_data:
+                self.order.remove(key)
 
-        self.cache_data[key] = item
-        self.order.append(key)
+            self.cache_data[key] = item
+            self.order.append(key)
 
-        if len(self.cache_data) > self.MAX_ITEMS:
-            most = self.order.pop(-2)
-            del self.cache_data[most]
-            print(f"DISCARD: {most}")
+            if len(self.cache_data) > self.MAX_ITEMS:
+                most = self.order.pop(-2)
+                del self.cache_data[most]
+                print(f"DISCARD: {most}")
 
     def get(self, key):
         """Retrieves an item from the cache using the given key.
