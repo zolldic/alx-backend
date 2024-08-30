f3c54f3d2d1d8bff9c0dede11b1e4ec6530c56b4
sai <ojiip95@gmail.com>
2024-08-30 18:20:53 +0200

    build: task 0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""a basic Flask app
+"""
+from flask import (Flask, render_template)
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Route handler for the root URL ('/').
+
+        This function is invoked when the root URL of
+        the application is accessed. It renders and returns
+        the HTML template '0-index.html'.
+
+        Returns:
+            str: The rendered HTML template '0-index.html'.
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
