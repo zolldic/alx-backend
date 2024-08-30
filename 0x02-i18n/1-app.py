776a83a71796ed9b8fcf41d044562240e35aeda5
sai <ojiip95@gmail.com>
2024-08-30 18:28:29 +0200

    refactor: render 1-index page

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -22,12 +22,12 @@ def index():
 
         This function is invoked when the root URL of
         the application is accessed. It renders and returns
-        the HTML template '0-index.html'.
+        the HTML template '1-index.html'.
 
         Returns:
-            str: The rendered HTML template '0-index.html'.
+            str: The rendered HTML template '1-index.html'.
     """
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
